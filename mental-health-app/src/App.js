19fbcf3b46a7603a140dcea603acf493ee7e0855
Altina <75257807+AltinaSalihu@users.noreply.github.com>
2025-02-26 10:28:51 +0100

    Fixed routes

--- mental-health-app/src/App.js
+++ mental-health-app/src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Index from "./views/Index"; // Importimi i vetëm i Index.js
+import Index from "./views/Index";
+import LandingPage from "./views/examples/Landing"; // Import Landing Page
+import ProfilePage from "./views/examples/Profile"; // Import Profile Page
+import LoginPage from "./views/examples/Login"; // Import Login Page
+import RegisterPage from "./views/examples/Register"; // Import Register Page
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Index />} /> {/* Rruga kryesore për Index */}
+          <Route path="/" element={<Index />} />
+          <Route path="/landing-page" element={<LandingPage />} />
+          <Route path="/profile-page" element={<ProfilePage />} />
+          <Route path="/register-page" element={<RegisterPage />} />
+          <Route path="/login-page" element={<LoginPage />} />
         </Routes>
       </Router>
     </div>
