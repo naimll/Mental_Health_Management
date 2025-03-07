60857c8046ffe1bb6b979d6766d7623f61d715a8
almanovoberdaliu <93130020+almanovoberdaliu@users.noreply.github.com>
2025-03-07 11:02:19 +0100

    connecting model with backend

---
+++ mhm-api/Models/MoodPredictionResult.cs
@@ -0,0 +1,7 @@
+﻿namespace mhm_api.Models
+{
+    public class MoodPredictionResult
+    {
+        public float Score { get; set; } // Parashikimi i humorit (p.sh., probabiliteti)
+    }
+}
---
+++ mhm-api/Service/MoodPredictionService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using System;
+using System.IO;
+using mhm_api.Models;
+using mhm_api.Models.mhm_api.Models;
+
+namespace mhm_api.Services
+{
+    public class MoodPredictionService
+    {
+        private readonly PredictionEngine<MoodEntry, MoodPredictionResult> _predictionEngine;
+        private static readonly string ModelPath = "mood_model.zip";
+
+        public MoodPredictionService()
+        {
+            var mlContext = new MLContext();
+            ITransformer trainedModel = mlContext.Model.Load(ModelPath, out var modelInputSchema);
+            _predictionEngine = mlContext.Model.CreatePredictionEngine<MoodEntry, MoodPredictionResult>(trainedModel);
+        }
+
+        public MoodPredictionResult PredictMood(MoodEntry input)
+        {
+            return _predictionEngine.Predict(input);
+        }
+    }
+}
