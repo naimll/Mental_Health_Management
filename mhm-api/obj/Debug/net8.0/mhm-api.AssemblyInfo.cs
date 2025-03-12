457b1c3753613d8f9af49b144f393c39d9ced5bb
Naim Llumnica <dev.dev5@ubt-uni.net>
2025-03-12 09:36:23 +0100

    .....

--- mhm-api/obj/Debug/net8.0/mhm-api.AssemblyInfo.cs
+++ mhm-api/obj/Debug/net8.0/mhm-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mhm-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92063f780adecd84ecfa3f53b768472d3dbc741d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+130ad1b896819d532183626d6a066c9f1e67b560")]
 [assembly: System.Reflection.AssemblyProductAttribute("mhm-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mhm-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
